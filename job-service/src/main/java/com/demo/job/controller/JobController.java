b1d0437f224c2b503157766006c510ac22831cb9
tuphungvan <cube438126@gmail.com>
2026-09-14 17:44:36 +0700

    feat/redis-rate-limiter: implement rate limit service by kong and redis fix window

--- job-service/src/main/java/com/demo/job/controller/JobController.java
+++ job-service/src/main/java/com/demo/job/controller/JobController.java
@@ -2,8 +2,11 @@ package com.demo.job.controller;
 
 import com.demo.job.domain.Job;
 import com.demo.job.service.JobService;
+import com.demo.job.service.RateLimiterService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,22 +19,29 @@ import java.util.Map;
 public class JobController {
 
     private final JobService jobService;
+    private final RateLimiterService rateLimiter;
 
     @PostMapping
-    public ResponseEntity<Map<String, Object>> create(
+    public ResponseEntity<Object> create(
             @RequestParam("name") String name,
-            @RequestParam("subJobCount") @Min(1) int subJobCount) {
+            @RequestParam("subJobCount") @Min(1) int subJobCount,
+            HttpServletRequest req) {
+
+        String clientIp = req.getHeader("X-Forwarded-For");
+        if (clientIp == null) clientIp = req.getRemoteAddr();
+        if (!rateLimiter.isAllowed(clientIp, 2, 60))
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("API rate limit exceeded");
         Job job = jobService.createJob(name, subJobCount);
         return ResponseEntity.ok(Map.of("message", "tạo job thành công, worker đang sử lý", "data", job));
     }
 
     @GetMapping
-    public ResponseEntity<List<Job>> get(){
+    public ResponseEntity<List<Job>> get() {
         return ResponseEntity.ok(jobService.getJob());
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Job> update(@PathVariable("id") Integer id){
+    public ResponseEntity<Job> update(@PathVariable("id") Integer id) {
         return ResponseEntity.accepted().body(jobService.updateJob(id));
     }
 }
